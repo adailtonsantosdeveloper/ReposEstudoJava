e74d82553c7209b546d34142cb676bd2361d9b5a
adailtonsantosdeveloper <127420850+adailtonsantosdeveloper@users.noreply.github.com>
2023-04-30 22:36:53 -0300

    Tentando commitar

---
+++ src/introducao/java/HeloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package introducao.java;
+
+/**
+ *
+ * @author ADAILTON
+ */
+public class HeloWorld {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        System.out.println("Java é uyma linguagem de programação multiplataforma.");
+    }
+    
+}
