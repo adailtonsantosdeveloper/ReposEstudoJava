7bbf14c4d0d7ce8942c5974e4262246f1387199d
adailtonsantosdeveloper <127420850+adailtonsantosdeveloper@users.noreply.github.com>
2023-05-01 20:38:51 -0300

    Create OperadoresAtribuicao.java

---
+++ src/operadores_atribuicao/OperadoresAtribuicao.java
@@ -0,0 +1,17 @@
+package operadores_atribuicao;
+
+public class OperadoresAtribuicao {
+    public static void main(String[] args) {
+        System.out.println("No Java, assim como em qualquer linguagem de programação" + 
+        "Há várias categorias de operadores, entre eles o operador de ATRIBUIÇÃO");
+        System.out.println(" em algumas linguagens são adotados \':=\' ou \'<-\' ou \'>>>\' ou \'>>\' e \'<<\'" +
+        "No Java o operador de atribuição usado é o \'=\'.");
+        System.out.println("Exemplo declarando uma variável: \n <tipoVariavel> <nomeVariavel> = <valor>;");
+        System.out.println("Vale lembrar que toda instrução em Java deve ser finalizada com \';\'");
+        System.out.println("\t int nInteiro = 10;");
+        System.out.println("Foi declarada uma varíável do tipo int com nome de nInteiro e receberá o valor 10");
+
+        int nInteiro = 10;
+    }
+    
+}
