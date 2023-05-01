c23526372f469baa3ce6cccfe12013ed6aa6a838
adailtonsantosdeveloper <127420850+adailtonsantosdeveloper@users.noreply.github.com>
2023-05-01 20:49:10 -0300

    Create OperadoresRelacionais.java

---
+++ src/operadores_relacionais/OperadoresRelacionais.java
@@ -0,0 +1,17 @@
+package operadores_relacionais;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        System.out.println("Operadores relacionais são operadores \"binários\", ou seja, executa uma comparação entre um" + 
+        " valor à esquerda com um valor à direita e retorna um valor booleano.");
+        System.out.println("Os operadores são os seguintes:");
+        System.out.println(">, >=, <, <=, != e ==");
+        System.out.println("A > B [A maior que B];");
+        System.out.println("A >= B [A maior ou igual B];");
+        System.out.println("A < B [A menor que B];");
+        System.out.println("A <= B [A menor ou igual B];");
+        System.out.println("A != B [A diferente de B];");
+        System.out.println("A == B [A igual B].");
+    }
+    
+}
