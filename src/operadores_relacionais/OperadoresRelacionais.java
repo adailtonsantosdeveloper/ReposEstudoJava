bccb4c8e6346a32fa795db9dc4d2698e3bc1e230
adailtonsantosdeveloper <127420850+adailtonsantosdeveloper@users.noreply.github.com>
2023-05-03 09:48:15 -0300

    Atualizando operadores relacionais

--- src/operadores_relacionais/OperadoresRelacionais.java
+++ src/operadores_relacionais/OperadoresRelacionais.java
@@ -7,11 +7,29 @@ public class OperadoresRelacionais {
         System.out.println("Os operadores são os seguintes:");
         System.out.println(">, >=, <, <=, != e ==");
         System.out.println("A > B [A maior que B];");
+        int A = 5;
+        int B = 8;
+        System.out.printf("A vale %d e B vale %d, A > B ? [true/false] = %b %n",A,B,(A > B));
         System.out.println("A >= B [A maior ou igual B];");
+        A = 6;
+        B = 5;
+        System.out.printf("A vale %d e B vale %d, A >= B ? [true/false] = %b %n",A,B,(A >= B));
         System.out.println("A < B [A menor que B];");
+        ++A;
+        B = (int)A/2;
+        System.out.printf("A vale %d e B vale %d, A <>> B ? [true/false] = %b %n",A,B,(A < B));
         System.out.println("A <= B [A menor ou igual B];");
+        A /= B;
+        B += A;
+        System.out.printf("A vale %d e B vale %d, A <= B ? [true/false] = %b %n",A,B,(A <= B));
         System.out.println("A != B [A diferente de B];");
+        A = 2;
+        B = 2;
+        System.out.printf("A vale %d e B vale %d, A != B ? [true/false] = %b %n",A,B,(A != B));
         System.out.println("A == B [A igual B].");
+        A = (int)Math.pow(A,2);
+        B = A * 2;
+        System.out.printf("A vale %d e B vale %d, A == B ? [true/false] = %b %n",A,B,(A == B));
     }
     
 }
