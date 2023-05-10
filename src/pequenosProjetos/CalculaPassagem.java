60666b332f137b3d6807052534a58759bf1648ee
adailtonsantosdeveloper <127420850+adailtonsantosdeveloper@users.noreply.github.com>
2023-05-10 09:24:13 -0300

    Praticando Java FX

--- src/pequenosProjetos/CalculaPassagem.java
+++ src/pequenosProjetos/CalculaPassagem.java
@@ -11,10 +11,14 @@ public class CalculaPassagem {
         String opcao = "";
         double valPassagem = 0;
         int quantPass = 0;
-        do {
-            System.out.println("Deseja calcular algum valor de passagem em quantidade? [S/N]");
-            opcao = entrada.nextLine();
-            if (opcao.equalsIgnoreCase("S")) {
+        System.out.println("Deseja calcular algum valor de passagem em quantidade? [S/N]");
+        opcao = entrada.nextLine();
+        char c = opcao.charAt(0);
+         
+        if (c == 's') {
+            do {
+            
+           
 
                 System.out.println("Por gentileza, informe o valor unitário da passagem: ");
                 valPassagem = Double.parseDouble(entrada.next());
@@ -28,27 +32,20 @@ public class CalculaPassagem {
                     System.out.printf("R$ %2.2f X %d = %2.2f", valPassagem, (cont + 1), valPassagem * (cont + 1));
                     System.out.println();
                 }
-                System.out.println("Deseja calcular outra passagem? [s/n]");
-                opcao = entrada.nextLine();
-
-            } else {
-                System.out.println("Já que não digitou S, vou assumir como se você tivesse digitado N.");
+                
                 opcao = "n";
-            }
+            } while (!opcao.equalsIgnoreCase ("n"));
             
-            if (!opcao.equalsIgnoreCase("s")) {
-                System.out.println("Obrigado por usar nosso programa.");
-                opcao = "n";
-            }
-            
-        } while (!opcao.equalsIgnoreCase ("n"));
+    else if (!opcao.equalsIgnoreCase("s")) {
+                
+        //opcao = "n";{
+        System.out.println("Já que não digitou S, vou assumir como se você tivesse digitado N.");
+        System.out.println("Obrigado por usar nosso programa.");
         entrada.close();
         System.out.println("Fim do programa!!!");
         System.out.println();
         System.out.println("----------------------------------------------------------------");
         System.out.println();
     }
-
-
-
+    }
 }
