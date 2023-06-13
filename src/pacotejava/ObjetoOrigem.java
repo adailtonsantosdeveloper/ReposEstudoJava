85510dca19c77ce5740f1f958c6991a179e37fe2
adailtonsantosdeveloper <127420850+adailtonsantosdeveloper@users.noreply.github.com>
2023-06-13 10:21:12 -0300

    Update ObjetoOrigem.java

--- src/pacotejava/ObjetoOrigem.java
+++ src/pacotejava/ObjetoOrigem.java
@@ -2,5 +2,25 @@ public class ObjetoOrigem {
 
     char []alfabedo = new char[128];
 
+    char letra(int posicao) {
+        alfabeto[0] = ' ';
+        alfabeto[1] = 'a';
+        alfabeto[2] = 'A';
+        alfabeto[3] = 'á';
+        alfabeto[4] = 'à';
+        alfabeto[5] = 'â';
+        alfabeto[6] = 'ã';
+        alfabeto[7] = 'Á';
+        alfabeto[8] = 'À';
+        alfabeto[9] = 'Â';
+        alfabeto[10]= 'Ã';
+        alfabeto[11]= '1';
+        alfabeto[12]= '2';
+        alfabeto[13]= '3';
+        alfabeto[14]= '4';
+        alfabeto[15]= '5';
 
+        
+
+    }
 }
