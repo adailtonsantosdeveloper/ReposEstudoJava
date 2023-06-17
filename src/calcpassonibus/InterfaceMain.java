6004130def07c6846e961877b925ade880ab8bf5
adailtonsantosdeveloper <127420850+adailtonsantosdeveloper@users.noreply.github.com>
2023-06-17 10:35:10 -0300

    alerar

--- src/calcpassonibus/InterfaceMain.java
+++ src/calcpassonibus/InterfaceMain.java
@@ -21,6 +21,31 @@ public class InterfaceMain {
         
         int quant_passagem = Integer.parseInt(entrada.next()); // Armazena o que o usuário digitar já convertendo em inteiro
         System.out.printf("%d foi a quantidade informada %n", quant_passagem);
+        System.out.println();
+        System.out.println("Com base nas informações fornecidas serão realizados os devidos cálculos...");
+
+        /* chama o método para multiplicar as passagens pela quantidade e armazena o valor na variável totalPassagem */
+        double totalPassagem = qPass.calcPassagem(quant_passagem, passagemFormatada);
+
+        /* Exibe o resultado */
+        System.out.printf("%d passagens a R$ %2.2f é = [ R$ %2.2f ]", quant_passagem, passagemFormatada, totalPassagem);
+
+        System.out.println("Informe quanto em dinheiro o cliente pagou:");
+        String valPago = entrada.nextLine();
+        String valPagoFormatado = vPass.verificaVirgula(valPago); // Se houver vírgula substituirá por ponto
+        double dinheiroPago = Double.parseDouble(valPagoFormatado);
+
+        if (dinheiroPago < totalPassagem) {
+            System.out.println("Está faltando dinheiro: ");
+            System.out.printf("%2.2f - 2.2f = %2.2f %n", dinheiroPago, totalPassagem, (dinheiroPago - totalPassagem));
+        } else {
+            double troco = qPass.calcTroco(dinheiroPago, totalPassagem);
+            String trocoFormatadoString = vPass.numberToString(troco);
+            trocoFormatadoString = vPass.botaVirgula(trocoFormatadoString);
+            System.out.printf("O troco de R$ %s para %d passagens de %s é R$ %s %n", valPago, quant_passagem, vPassagem, trocoFormatadoString);
+        }
+
+        entrada.close();
         
 
 
--- src/calcpassonibus/ValorPassagem.java
+++ src/calcpassonibus/ValorPassagem.java
@@ -1,12 +1,22 @@
+
 public class ValorPassagem {
     String verificaVirgula(String preco) {
         
-        if (preco.indexOf(",")) {
+        if (preco.indexOf(',')) {
             preco.replaceAll(",", ".");
         }
         String precoCorrigido = preco;
 
         return precoCorrigido;
+    }
+
+    String botaVirgula (String preco) {
+        if (preco.indexOf('.')) {
+            preco.replaceAll(".", ",");
+        }
+        String precoComVirgula = preco;
+
+        return precoComVirgula;
     }
 
     double stringToNumber ( String preco) {
